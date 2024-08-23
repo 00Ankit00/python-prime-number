58dee19e17e31f344b95e5820b87f1c61e9f39fe
00Ankit00 <rayroni2005@gmail.com>
2024-08-23 21:05:58 +0530

    Add files via upload

---
+++ prime_number.py
@@ -0,0 +1,12 @@
+number=int(input("enter number: "))
+composite=0
+for i in range(2,number):
+    if(number%i==0):
+        composite+=1  
+if(composite==0):
+    if(number==0 or number==1):
+        print("the number entered is composite")
+    else:
+        print("the number entered is prime")
+else:
+    print("the number entered is composite")
